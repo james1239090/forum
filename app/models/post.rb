1ba1b7b11f1e577e7cd4b3f681484190726dafd6
James <james1239090@gmail.com>
2017-04-01 19:04:34 +0800

    implement like to Post for current user and counts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  has_many :likes
 
   validates :content, presence: true
 
   scope :recent, -> {order("created_at DESC")}
+  def is_like?(user)
+    self.likes.where(:user_id => user.id).length > 0
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328084140) do
+ActiveRecord::Schema.define(version: 20170401094940) do
 
   create_table "group_relationships", force: :cascade do |t|
     t.integer  "group_id"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20170328084140) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
