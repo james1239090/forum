c69c162b036f1f88c66331107dadb753e420f501
James <james1239090@gmail.com>
2017-04-07 15:02:28 +0800

    require authenticate_user on join and quit action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,8 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create,:edit,:update,:destroy]
+  before_action :authenticate_user!, only: [:new,:create,:edit,:update,:destroy, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
-    @groups = Group.includes(:user).all
+    @groups = Group.all
   end
 
   def new
