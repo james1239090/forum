c11f811983abfc4cccc4f811f890fd10f77f05bc
James <james1239090@gmail.com>
2017-04-07 14:46:25 +0800

    add the paginate to like and post of account page

--- app/controllers/account/likes_controller.rb
+++ app/controllers/account/likes_controller.rb
@@ -1,5 +1,5 @@
 class Account::LikesController < ApplicationController
   def index
-    @posts =  current_user.like_posts.includes(:user,:group)
+    @posts =  current_user.like_posts.includes(:user,:group).recent.paginate(:page=> params[:page], :per_page=> 5)
   end
 end
--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -2,7 +2,7 @@ class Account::PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = current_user.posts.includes(:group).recent
+    @posts = current_user.posts.includes(:group).recent.paginate(:page=> params[:page], :per_page=> 5)
   end
 
 
