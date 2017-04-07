e297e43f2c8548ff31085d406d1a60791d246705
James <james1239090@gmail.com>
2017-04-07 15:57:16 +0800

    add :dependent => :destroy on Group, Post, Commet, Like Model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 class Group < ApplicationRecord
-  has_many :posts
+  has_many :posts,:dependent => :destroy
   belongs_to :user
   validates :title, presence: true
 
-  has_many :group_relationships
+  has_many :group_relationships,:dependent => :destroy
   has_many :member, through: :group_relationships, source: :user
 end
